bc130022771a49d02f2e096b40f44d8367cb7bc2
JFAlvarez <jfalvarez80@outlook.com>
2021-08-13 13:59:27 +0200

    Mejoras manifest Base Plugins

--- CNimbus_BasePlugins/__manifest__.py
+++ CNimbus_BasePlugins/__manifest__.py
@@ -5,8 +5,9 @@
     'version': '13.0.1.0',
     'category': 'Tools',
     'depends': ['auto_backup','disable_odoo_online','remove_odoo_enterprise','web_decimal_numpad_dot'],
-    'description': "Módulos básicos para cualquier instalación de Odoo",
+    'description': 'Módulos básicos para cualquier instalación de Odoo',
     'installable': True,
     'auto_install': False,
     'license': 'LGPL-3',
+    'external_dependencies': {'python': ['pysftp']},
 }
