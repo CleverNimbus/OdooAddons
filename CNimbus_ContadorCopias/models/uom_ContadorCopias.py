44b7a2abd59f58416d6eda815fd64db02f048d7b
JFAlvarez <jfalvarez80@outlook.com>
2021-08-13 13:54:59 +0200

    Mejoras de UOM

--- CNimbus_ContadorCopias/models/uom_ContadorCopias.py
+++ CNimbus_ContadorCopias/models/uom_ContadorCopias.py
@@ -5,4 +5,8 @@ from odoo import models, fields
 class UomCategory(models.Model):
     _inherit = "uom.category"
 
-    measure_type = fields.Selection(selection_add=[('copies', 'Copies')])
+    measure_type = fields.Selection(selection_add=[('copiesA4BN', 'Copies A4 BN'),
+                                                   ('copiesA4Color', 'Copies A4 Color'),
+                                                   ('copiesA3BN', 'Copies A3 BN'),
+                                                   ('copiesA3Color', 'Copies A3 Color'),
+                                                   ('copiesBunner', 'Copies Banner')])
